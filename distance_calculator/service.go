7ef5ab48acab1cd668e8289ac0372ad8eaa5bdf7
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-09 19:51:59 +0200

    [calculator] calculator service calculate distance implementation using map for storing last point from obu

--- distance_calculator/service.go
+++ distance_calculator/service.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"math"
 
 	"github.com/Hhanri/toll_calculator/types"
 )
@@ -10,14 +10,32 @@ type CalculatorServicer interface {
 	CalculateDistance(types.ObuData) (float64, error)
 }
 
-type CalculatorService struct{}
+type CalculatorService struct {
+	points map[int]types.Geo
+}
 
 func NewCalculatorService() *CalculatorService {
-	return &CalculatorService{}
+	return &CalculatorService{
+		points: make(map[int]types.Geo),
+	}
 }
 
 func (s *CalculatorService) CalculateDistance(data types.ObuData) (float64, error) {
-	fmt.Println("calculating the distance")
+	defer s.saveLastPoint(data)
+	prev, ok := s.points[data.ObuId]
+	if !ok {
+		return 0.0, nil
+	}
+	distance := calculateDistance(data.Geo.Lng, data.Geo.Lat, prev.Lng, prev.Lat)
+	return distance, nil
+}
 
-	return 0.0, nil
+func (s *CalculatorService) saveLastPoint(data types.ObuData) {
+	s.points[data.ObuId] = data.Geo
+}
+
+func calculateDistance(x1, y1, x2, y2 float64) float64 {
+	a := math.Pow(x2-x1, 2)
+	b := math.Pow(y2-y1, 2)
+	return math.Sqrt(a + b)
 }
