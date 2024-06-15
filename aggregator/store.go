6554827438d432f97f27e51a8dc5d443dabae275
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-15 16:57:54 +0200

    [aggregator] memory store implementation

--- aggregator/store.go
+++ aggregator/store.go
@@ -6,12 +6,17 @@ type Storer interface {
 	Insert(types.Distance) error
 }
 
-type MemoryStore struct{}
+type MemoryStore struct {
+	data map[int64]float64
+}
 
 func NewMemoryStore() Storer {
-	return &MemoryStore{}
+	return &MemoryStore{
+		data: make(map[int64]float64),
+	}
 }
 
 func (m *MemoryStore) Insert(distance types.Distance) error {
+	m.data[int64(distance.ObuId)] += (distance.Value)
 	return nil
 }
