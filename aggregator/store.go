20f96ebc3fdac1fcbe6d8ecf1aa68b90ff78b2a9
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-15 16:09:17 +0200

    [aggregator] memory store placeholder

--- aggregator/store.go
+++ aggregator/store.go
@@ -5,3 +5,13 @@ import "github.com/Hhanri/toll_calculator/types"
 type Storer interface {
 	Insert(types.Distance) error
 }
+
+type MemoryStore struct{}
+
+func NewMemoryStore() Storer {
+	return &MemoryStore{}
+}
+
+func (m *MemoryStore) Insert(distance types.Distance) error {
+	return nil
+}
