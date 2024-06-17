d9e80d06599df1750ec752d8dc931c1c485ead15
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-17 15:20:35 +0200

    [aggregator] store changed id int64 to int

--- aggregator/store.go
+++ aggregator/store.go
@@ -8,25 +8,25 @@ import (
 
 type Storer interface {
 	Insert(types.Distance) error
-	Get(int64) (float64, error)
+	Get(int) (float64, error)
 }
 
 type MemoryStore struct {
-	data map[int64]float64
+	data map[int]float64
 }
 
 func NewMemoryStore() Storer {
 	return &MemoryStore{
-		data: make(map[int64]float64),
+		data: make(map[int]float64),
 	}
 }
 
 func (m *MemoryStore) Insert(distance types.Distance) error {
-	m.data[int64(distance.ObuId)] += (distance.Value)
+	m.data[distance.ObuId] += (distance.Value)
 	return nil
 }
 
-func (m *MemoryStore) Get(id int64) (float64, error) {
+func (m *MemoryStore) Get(id int) (float64, error) {
 	dist, ok := m.data[id]
 	if !ok {
 		return 0.0, fmt.Errorf("could not find distance for obu [%d]", id)
