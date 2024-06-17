e90db050d3002e2f778b2d43e7d6a267a264c8e8
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-17 15:16:23 +0200

    [aggregator] store get

--- aggregator/store.go
+++ aggregator/store.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/Hhanri/toll_calculator/types"
+import (
+	"fmt"
+
+	"github.com/Hhanri/toll_calculator/types"
+)
 
 type Storer interface {
 	Insert(types.Distance) error
+	Get(int64) (float64, error)
 }
 
 type MemoryStore struct {
@@ -19,4 +24,12 @@ func NewMemoryStore() Storer {
 func (m *MemoryStore) Insert(distance types.Distance) error {
 	m.data[int64(distance.ObuId)] += (distance.Value)
 	return nil
+}
+
+func (m *MemoryStore) Get(id int64) (float64, error) {
+	dist, ok := m.data[id]
+	if !ok {
+		return 0.0, fmt.Errorf("could not find distance for obu [%d]", id)
+	}
+	return dist, nil
 }
